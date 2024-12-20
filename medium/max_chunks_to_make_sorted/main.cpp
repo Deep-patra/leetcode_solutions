19643e30bbf9cc3ceb786704766fb4c34ea26ffd
Deep patra <deeppatra1999@gmail.com>
2024-12-20 14:43:48 +0530

    fix: removed generated output file

--- medium/max_chunks_to_make_sorted/main.cpp
+++ medium/max_chunks_to_make_sorted/main.cpp
@@ -1,4 +1,67 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main() { return 0; }
+int max_chunks_to_make_sorted(vector<int> nums) {
+
+  /**
+    the logic of this problem is simple.
+
+    - Keep increasing the partitions when there is a sorted elements.
+    - Increase the paritions by 1, when there is a unsorted elements.
+  **/
+
+  int partitions = 1;
+
+  int max_num = INT_MIN; // maximum number in the patition
+  int i = 1;
+
+  while (i < nums.size()) {
+    if (nums[i - 1] > nums[i]) {
+      max_num = max(nums[i - 1], nums[i]);
+
+      int j = i;
+      while (j < nums.size() && max_num > nums[j])
+        j++;
+
+      i = j;
+    } else {
+
+      i++;
+      partitions++;
+    }
+  }
+
+  return partitions;
+}
+
+int main() {
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int test_cases = 0;
+  cin >> test_cases;
+
+  for (int i = 0; i < test_cases; i++) {
+    int length = -1;
+    cin >> length;
+
+    vector<int> nums;
+
+    while (length > 0) {
+      length--;
+
+      int num = 0;
+      cin >> num;
+
+      nums.push_back(num);
+    }
+
+    int result = max_chunks_to_make_sorted(nums);
+    cout << result << '\n';
+  }
+
+  return 0;
+}
---
+++ medium/sort_jumbled_numbers/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> sort_jumbled(vector<int> &mapping, vector<int> &nums) {}
+
+int main() { return 0; }
